6f52b480eda9f950ea107924bca3c041e07e59ae
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-08-04 18:55:58 -0400

    added a HashTable class and a bunch of methods

--- training/get_hash.py
+++ training/get_hash.py
@@ -1,7 +1,28 @@
-def get_hash(key):
-    h = 0
-    for char in key:
-        h += ord(char)
-    return h % 100
+class HashTable:
+    def __init__(self):
+        self.Max = 100
+        self.arr = [None for i in range(self.Max)]
+  
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % 100
+    
+    def add(self, key, val):
+        h = self.get_hash(key)
+        self.arr[h] = val
+    
+    def get(self, key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+    def delete(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None
 
-print(get_hash('vlad'))
+t = HashTable()
+
+print(t.get_hash('vlad'))
+print(t.get_hash('vasya'))
+print(t.get_hash('vova'))

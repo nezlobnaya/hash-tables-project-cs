a3c472befd65fdddbcb643aadaeec1b80401c6d6
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-08-10 19:06:14 -0400

    word_count added

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,20 @@
 def word_count(s):
     # Your code here
+    lowercase = s.lower()
+    count = {}
+    ignore = '":;,.-+=/\\|[]{}()*^&'
+    for char in lowercase:
+        if char in ignore:
+            lowercase = lowercase.replace(char,"")
+    words = lowercase.split()
+    for word in words:
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+    return count
 
+   
 
 
 if __name__ == "__main__":
@@ -8,3 +22,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
